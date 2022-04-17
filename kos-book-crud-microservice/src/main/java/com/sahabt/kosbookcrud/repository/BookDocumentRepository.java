5756e7b17b7024ce52fc3436913b2853affa9dae
Remzi ŞAHBAZ <75884873+RShbz@users.noreply.github.com>
2022-04-18 02:15:24 +0300

    Delete kos-book-crud-microservice/src/main/java/com/sahabt/kosbookcrud directory

--- kos-book-crud-microservice/src/main/java/com/sahabt/kosbookcrud/repository/BookDocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sahabt.kosbookcrud.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.sahabt.kosbookcrud.document.BookDocument;
-
-public interface BookDocumentRepository extends MongoRepository<BookDocument,String>{
-
-}
